9ad4d681cc684f2afb7598ab43018d08312d155f
Sergej Garkusha <sergejgarkusha94@gmail.com>
2025-03-31 12:56:32 +0200

    reset project

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export default function Page() {
-	return (
-		<View className='flex-1 items-center justify-center bg-blue-500'>
-			<Text className='text-white text-lg'>Hello Tailwind!</Text>
-		</View>
-	)
-}
